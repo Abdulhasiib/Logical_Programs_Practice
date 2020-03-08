ec1f83a7385a148c13bccf76035cd27d0c35d363
Mohammed Haseeb <Mohammed.Haseeb@aveva.com>
2020-03-08 20:13:35 +0530

    Output text based on divisible by 3 or 5

---
+++ ConsoleApp1/Output a text based on even odd.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Output_a_text_based_on_even_odd
+    {
+        static void Main(String[] args)
+        {
+            //User Input for maximum number
+            Console.WriteLine("Enter Maximum number: ");
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            string state = "";
+
+            for (int i = 1; i < n + 1; i++)
+            {
+                if ((i % 3) == 0 && (i % 5) == 0)
+                {
+                    state = "FizzBuzz";
+                }
+                else if ((i % 3) == 0 && (i % 5) != 0)
+                {
+                    state = "Fizz";
+                }
+                else if ((i % 5) == 0 && (i % 3) != 0)
+                {
+                    state = "Buzz";
+                }
+                else
+                {
+                    state = "number";
+                }
+
+                switch (state)
+                {
+                    case "number":
+                        Console.WriteLine(i);
+                        break;
+
+                    case "Fizz":
+                        Console.WriteLine("Fizz");
+                        break;
+
+                    case "Buzz":
+                        Console.WriteLine("Buzz");
+                        break;
+
+                    case "FizzBuzz":
+                        Console.WriteLine("FizzBuzz");
+                        break;
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
