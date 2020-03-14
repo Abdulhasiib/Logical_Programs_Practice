b1817e70b3a4b7cceef0a7d27b094d2d9b94bfd4
Mohammed Haseeb <Mohammed.Haseeb@aveva.com>
2020-03-14 12:41:21 +0530

    ternaroy operator

---
+++ ConsoleApp1/Divisible_By_3_or_5_or_both_Ternary_operator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Divisible_By_3_or_5_or_both_Ternary_operator
+    {
+        static void Main(String[] Args)
+        {
+            Console.WriteLine("Enter Maximum Number");
+
+            int MaxNumber = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for(int i = 1; i <= MaxNumber; i++)
+            {
+                string Result = ((i % 3 == 0) && (i % 5 == 0)) ? "FizzBuzz" : (i % 3 == 0) ? "Fizz" : (i % 5 == 0) ? "Buzz" : i.ToString();
+
+                Console.WriteLine(Result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
