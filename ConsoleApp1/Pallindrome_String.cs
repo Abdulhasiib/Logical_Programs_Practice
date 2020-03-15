3a3fd4f28b6bb3c97a8bcaedfe6c4d0ab5d9c865
Mohammed Haseeb <Mohammed.Haseeb@aveva.com>
2020-03-15 15:13:36 +0530

    Palindrome String

---
+++ ConsoleApp1/Pallindrome_String.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Pallindrome_String
+    {
+        static void Main(String[] Args)
+        {
+            Console.WriteLine("Enter String: ");
+
+            string InputString = Console.ReadLine();
+
+            int min = 0;
+            int max = InputString.Length - 1;
+            bool flag = true;
+
+            while (flag)
+            {
+                if (min > max)
+                {
+                    Console.WriteLine("String {0} is Palindrome.", InputString);
+                    break;
+                }
+                char a = InputString[min];
+                char b = InputString[max];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    Console.WriteLine("String {0} is not Palindrome.", InputString);
+                    flag = false;
+                }
+                min++;
+                max--;
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
