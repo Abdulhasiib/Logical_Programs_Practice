db0e2994554269866215c7e550370438a4276af0
Mohammed Haseeb <Mohammed.Haseeb@aveva.com>
2020-04-04 09:45:57 +0530

    Leet01 Two Sum

---
+++ ConsoleApp1/Leet_01_Two_Sum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Leet_01_Two_Sum
+    {
+        public static void Main(String[] args)
+        {
+            int[] vnums = { 1, 6, 4, 9 };
+            int target = 10;
+            int[] res = TwoSum(vnums, target);
+            foreach (var item in res)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadLine();
+        }
+
+        public static int[] TwoSum(int[] nums, int Target)
+        {
+            int sum = 0;
+            int[] result = { 0, 0};
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    sum = nums[i] + nums[j];
+
+                    if (sum == Target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                        return result;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp1/ReversePalindrome.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ReversePalindrome
+    {
+        static void Main(String[] Args)
+        {
+            string InputString = " ", FirstMatched = "Matched", FirstUnMatched = "UnMatched";
+            bool IsMatched = false;
+            int i, j;
+            Console.WriteLine("Enter string: ");
+
+            InputString = Console.ReadLine();
+
+            char[] String_Array = InputString.ToCharArray();
+
+            for (i = 0; i < String_Array.Length; i++) // i = J
+            {
+                for (j = 0; j < String_Array.Length; j++) // j = a,v,a
+                {
+                    if (i != j && String_Array[i] == String_Array[j]  && FirstMatched == "Matched")
+                    {
+                        FirstMatched = Convert.ToString(String_Array[i]);
+                        break;
+                    }
+                    else if ( j == String_Array.Length - 1 && FirstUnMatched == "UnMatched")
+                    {
+                        FirstUnMatched = Convert.ToString(String_Array[i]);
+                        break;
+                    }
+                }
+                
+
+                if (FirstMatched != "Matched" && FirstUnMatched != "UnMatched")
+                {
+                    break;
+                }
+                
+            }
+
+            
+            Console.WriteLine("First Matched letter is {0}", FirstMatched);
+            Console.WriteLine("First UnMatched letter is {0}", FirstUnMatched);
+            Console.ReadLine();
+        }
+    }
+}
