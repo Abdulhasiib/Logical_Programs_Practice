bf68623b031895b9ecc6c1e55193fafa3329c0d9
Mohammed Haseeb <Mohammed.Haseeb@aveva.com>
2020-04-04 17:16:25 +0530

    Leet Code 01 - Dictionary Solution

--- ConsoleApp1/Leet_01_Two_Sum.cs
+++ ConsoleApp1/Leet_01_Two_Sum.cs
@@ -10,9 +10,11 @@ namespace ConsoleApp1
     {
         public static void Main(String[] args)
         {
-            int[] vnums = { 1, 6, 4, 9 };
+            int[] vnums = { 2, 7, 11, 15 };
             int target = 10;
-            int[] res = TwoSum(vnums, target);
+            //int[] res = TwoSum(vnums, target);
+
+            int[] res = DictTwoSum(vnums, target);
             foreach (var item in res)
             {
                 Console.Write(item + " ");
@@ -39,6 +41,23 @@ namespace ConsoleApp1
                 }
             }
             return result;
+        }
+
+        public static int[] DictTwoSum(int[] nms, int trgt)
+        {
+            Dictionary<int, int> inputArrayDict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nms.Length; i++)
+            {
+                int complimentNu = trgt - nms[i];
+                if (inputArrayDict.ContainsKey(complimentNu))
+                {
+                    int[] resultArray = { inputArrayDict[complimentNu],i  };
+                    return resultArray;
+                }
+                inputArrayDict.Add(nms[i],i);
+            }
+            throw new Exception("No two sum solution");
         }
     }
 }
