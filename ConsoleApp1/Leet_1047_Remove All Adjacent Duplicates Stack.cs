be9efbdbb96ef8c5bcab5a7a694ceff561d99393
Mohammed Haseeb <Mohammed.Haseeb@aveva.com>
2020-12-30 20:25:43 +0530

    Solved Leet Code Problem number 1047

---
+++ ConsoleApp1/Leet_1047_Remove All Adjacent Duplicates Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Leet_1047_Remove_All_Adjacent_Duplicates_Stack
+    {
+        public static void Main(string[] Args)
+        {
+            Console.WriteLine("Enter input string: ");
+            string inputstr = Console.ReadLine();
+            
+            Stack<char> temp = new Stack<char>();
+            
+            string output = "";
+            for (int i = 0; i < inputstr.Length; i++)
+            {
+                if (temp.Count>0 && temp.Peek() == inputstr[i])
+                {
+                    temp.Pop();
+                }
+                else
+                {
+                    temp.Push(inputstr[i]);
+                }
+                
+            }
+            foreach(char letter in temp)
+            {
+                output = letter + output;
+            }
+            
+            Console.WriteLine(output);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp1/Leet_1047_Remove All Adjacent Duplicates in String.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Leet_1047_Remove_All_Adjacent_Duplicates_in_String
+    {
+        public static void Main(string[] Args)
+        {
+            Console.WriteLine("Enter input string: ");
+
+            string inputstr = Console.ReadLine();
+            bool duplicate;
+            do
+            {
+                duplicate = false;
+
+                for (int i=0; i<inputstr.Length-1; i++)
+                {
+                    if(inputstr[i] == inputstr[i + 1])
+                    {
+                        inputstr = inputstr.Remove(i,2);
+                        duplicate = true;
+                    }
+                }
+                
+            } while (duplicate == true);
+
+            Console.WriteLine(inputstr);
+            Console.ReadLine();
+
+        }
+    }
+}
