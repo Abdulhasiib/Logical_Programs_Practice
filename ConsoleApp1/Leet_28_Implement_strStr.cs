986b70e8f70b9e1c1c03acd8a5b65a865cf4e4cd
Mohammed Haseeb <Mohammed.Haseeb@aveva.com>
2021-01-10 19:14:08 +0530

    LeetCode problem number 28 solved

---
+++ ConsoleApp1/Leet_28_Implement_strStr.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Leet_28_Implement_strStr
+    {
+        public static void Main(string[] args)
+        {
+            string haystack = "hello";
+            string needle = "ll";
+            int output = Leet_28_Implement_strStr.StrStr(haystack, needle);
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+            
+        }
+
+        public static int StrStr(string haystack, string needle)
+        {
+
+            if (!haystack.Contains(needle))
+                return -1;
+            else if (haystack == string.Empty || needle == string.Empty)
+                return 0;
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (haystack[i] == needle[0])
+                {
+                    string temp = haystack.Substring(i, needle.Length);
+
+                    if (temp == needle)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
